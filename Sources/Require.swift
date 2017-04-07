17309a075aa893d0e61823a9b131fc93ef18a673
John Sundell <john@sundell.co>
2017-04-07 18:17:10 +0200

    Add documentation

--- Sources/Require.swift
+++ Sources/Require.swift
@@ -1,6 +1,23 @@
+/**
+ *  Require
+ *  Copyright (c) John Sundell 2017
+ *  Licensed under the MIT license. See LICENSE file.
+ */
+
 import Foundation
 
 public extension Optional {
+    /**
+     *  Require this optional to contain a non-nil value
+     *
+     *  This method will either return the value that this optional contains, or trigger
+     *  a `preconditionFailure` with an error message containing debug information.
+     *
+     *  - parameter hint: Optionally pass a hint that will get included in any error
+     *                    message generated in case nil was found.
+     *
+     *  - return: The value this optional contains.
+     */
     func require(hint hintExpression: @autoclosure () -> String? = nil,
                  file: StaticString = #file,
                  line: UInt = #line) -> Wrapped {
