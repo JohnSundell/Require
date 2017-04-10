16294fb7a717b5eb2c97e18e311fc19a2d9a66e2
Alessio Roberto <darthpelo@gmail.com>
2017-04-10 19:22:10 +0200

    🐧  Rise NSException only if os is not Linux

--- Sources/Require.swift
+++ Sources/Require.swift
@@ -23,21 +23,21 @@ public extension Optional {
                  line: UInt = #line) -> Wrapped {
         guard let unwrapped = self else {
             var message = "Required value was nil in \(file), at line \(line)"
-
+            
             if let hint = hintExpression() {
                 message.append(". Debugging hint: \(hint)")
             }
-
-            let exception = NSException(
-                name: .invalidArgumentException,
-                reason: message,
-                userInfo: nil
-            )
-
+            #if !os(Linux)
+                let exception = NSException(
+                    name: .invalidArgumentException,
+                    reason: message,
+                    userInfo: nil
+                )
+            #endif
             exception.raise()
             preconditionFailure(message)
         }
-
+        
         return unwrapped
     }
 }
