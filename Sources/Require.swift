44d526601e81dc169766c429e27405c5adfdcbf2
John Sundell <john@sundell.co>
2017-04-11 23:17:09 +0200

    Remove whitespace & improve code formatting

--- Sources/Require.swift
+++ Sources/Require.swift
@@ -23,20 +23,24 @@ public extension Optional {
                  line: UInt = #line) -> Wrapped {
         guard let unwrapped = self else {
             var message = "Required value was nil in \(file), at line \(line)"
+
             if let hint = hintExpression() {
                 message.append(". Debugging hint: \(hint)")
             }
+
             #if !os(Linux)
-                let exception = NSException(
-                    name: .invalidArgumentException,
-                    reason: message,
-                    userInfo: nil
-                )
-                exception.raise()
+            let exception = NSException(
+                name: .invalidArgumentException,
+                reason: message,
+                userInfo: nil
+            )
+
+            exception.raise()
             #endif
+
             preconditionFailure(message)
         }
-        
+
         return unwrapped
     }
 }
