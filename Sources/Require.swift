7e4b03da2e8cd294938dcf9f5df89a332d347712
Alessio Roberto <darthpelo@gmail.com>
2017-04-11 23:07:12 +0200

    Remove added whitespace

--- Sources/Require.swift
+++ Sources/Require.swift
@@ -23,7 +23,6 @@ public extension Optional {
                  line: UInt = #line) -> Wrapped {
         guard let unwrapped = self else {
             var message = "Required value was nil in \(file), at line \(line)"
-            
             if let hint = hintExpression() {
                 message.append(". Debugging hint: \(hint)")
             }
